9c82700a5d70ed5aeb0d98ccb346e32ca7885bf9
Martin Dahlgren <martin_dahlgren@icloud.com>
2016-10-05 21:13:31 +0200

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -8,8 +8,8 @@ mkdir -p _assets/website/
 mkdir -p _assets/ebook/
 
 # Compile JS
-browserify src/js/core/index.js | uglifyjs -b > _assets/website/gitbook.js
-browserify src/js/theme/index.js | uglifyjs -b > _assets/website/theme.js
+browserify src/js/core/index.js | uglifyjs -b -nm -ns > _assets/website/gitbook.js
+browserify src/js/theme/index.js | uglifyjs -b -nm -ns > _assets/website/theme.js
 
 # Compile Website CSS
 lessc -clean-css src/less/website.less _assets/website/style.css
